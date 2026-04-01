6bd348c9d59b15acfa088263f026263206b069b4
Mateo Rodriguez <mateor861@gmail.com>
2026-04-01 17:21:38 -0300

    Fix: Exposed env variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,17 +7,18 @@ import { authInterceptor } from './auth/interceptors/auth.interceptors';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/enviroment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), 
+    provideRouter(routes),
     provideHttpClient(
       withFetch(),
       withInterceptors([
         authInterceptor,
       ])
-    ), provideFirebaseApp(() => initializeApp({ projectId: "createportfolio-31018", appId: "1:797063378849:web:85f7e547d360a406e27de0", storageBucket: "createportfolio-31018.firebasestorage.app", apiKey: "AIzaSyClmHAuGaD2Y6WN0Lwljhx2uBBPZSdYYbw", authDomain: "createportfolio-31018.firebaseapp.com", messagingSenderId: "797063378849", measurementId: "G-HYL0TJNHD5"})), provideAuth(() => getAuth()), provideStorage(() => getStorage()),
+    ), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth()), provideStorage(() => getStorage()),
   ]
 };
