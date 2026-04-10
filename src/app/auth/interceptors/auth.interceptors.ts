b3f4f00dc4ec4ef5336db07913cbdc9114c8665e
Mateo Rodriguez <mateor861@gmail.com>
2026-04-09 21:02:01 -0300

    Fix: Auth Interceptor

--- src/app/auth/interceptors/auth.interceptors.ts
+++ src/app/auth/interceptors/auth.interceptors.ts
@@ -1,14 +1,19 @@
 import { HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { from, switchMap } from 'rxjs';
 
-export function authInterceptor(
-  req: HttpRequest<unknown>,
-  next: HttpHandlerFn
-) {
-  const token = localStorage.getItem('token')
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const auth = inject(Auth);
 
-  const newReq = req.clone({
-    headers: req.headers.append('Authorization', `Bearer ${token}`),
-  });
-  return next(newReq);
+  if (!auth.currentUser) return next(req);
+
+  return from(auth.currentUser.getIdToken()).pipe(
+    switchMap(token => {
+      const newReq = req.clone({
+        headers: req.headers.append('Authorization', `Bearer ${token}`),
+      });
+      return next(newReq);
+    })
+  );
 }
